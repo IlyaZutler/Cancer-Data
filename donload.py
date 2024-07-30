0f1d677bf7fecd9abf03330b3434720893823812
IlyaZutler <izutler@gmail.com>
2024-07-30 08:44:00 +0300

    щл

--- donload.py
+++ donload.py
@@ -9,12 +9,14 @@ cache_file_path = r'C:\Users\domashniy\Documents\GitHub\Project_3-Airbnb\cached_
 
 # Функция для чтения Excel-файла и кэширования DataFrame
 def read_excel_with_cache(excel_path, cache_path):
+
     if os.path.exists(cache_path):
         # Если кэш-файл существует, загружаем DataFrame из кэша
         with open(cache_path, 'rb') as cache_file:
             df = pickle.load(cache_file)
         print("DataFrame загружен из кэша.")
     else:
+
         # Если кэш-файл не существует, читаем Excel-файл и сохраняем в кэш
         df = pd.read_excel(excel_path, engine='openpyxl')
         # Если ваш файл в формате .xls, используйте engine='openpyxl'
@@ -27,10 +29,16 @@ def read_excel_with_cache(excel_path, cache_path):
 df = read_excel_with_cache(excel_file_path, cache_file_path)
 
 # Вывод первых нескольких строк DataFrame для проверки
-print(df.head())
+print(df.head(1))
 print(df.shape)
+print(df.info())
+print(df.isna().sum())
+print(df.describe())
 
 apartments = df.drop(columns=['index', 'Review ID', 'review_date','Reviewer ID', 'Reviewer Name','Comments' ]).drop_duplicates()
+
+apartments.to_excel(r'C:\Users\domashniy\Documents\GitHub\Project_3-Airbnb\apartments.xlsx', index=False)
+
 print(apartments.head(1))
 print(apartments.shape)
 
