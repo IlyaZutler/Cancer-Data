15740863109deed3722254db676131ebdf6ed478
IlyaZutler <izutler@gmail.com>
2024-08-24 14:43:21 +0300

    ok

--- 0 Data Preparation.py
+++ 0 Data Preparation.py
@@ -21,34 +21,35 @@ df = read_excel_with_cache(excel_file_path, cache_file_path)
 
 df['Price'] = df['Price'].str.replace('$', '').str.replace(',', '').astype(float)
 df['Postal Code'] = df['Postal Code'].str.replace('.0', '').astype(str)
+
 apartments = df.drop(columns=['index', 'Review ID', 'review_date','Reviewer ID', 'Reviewer Name', 'Comments',
                               'Listing URL', 'Listing Name', 'Host ID', 'Host URL', 'Host Name',
-                              'Country Code', City, 'Country', 'Business Travel Ready']).drop_duplicates()
+                              'Country Code', 'City', 'Country', 'Business Travel Ready']).drop_duplicates()
 
-#apartments.to_excel(r'C:\Users\domashniy\Documents\GitHub\Project_3-Airbnb\Project-3-Berlin-Airbnb-Ratings\apartments.xlsx', index=False)
+apartments.to_excel(r'C:\Users\domashniy\Documents\GitHub\Project_3-Airbnb\apartments.xlsx', index=False)
+
 
 def remove_host_name_from_comments(row):
     comment = row['Comments']
     host_name = row['Host Name']
 
-    # Обрабатываем случаи, когда значение может быть None или NaN
     if pd.isna(comment) or pd.isna(host_name):
         return comment
 
-    # Удаляем слово хоста из комментария
     updated_comment = comment.replace(str(host_name), '').strip()
     return updated_comment
 
-# Создаем новый датафрейм с результатом
+
 df_с = pd.DataFrame()
 df_с['Comments'] = df.apply(remove_host_name_from_comments, axis=1)
 
-# Удаляем строки, где 'Comments1' пустой или состоит только из пробелов
 df_с = df_с[df_с['Comments'].str.strip() != '']
 df_с.dropna(inplace=True)
 
-# df_с.to_excel(r'C:\Users\domashniy\Documents\GitHub\Project_3-Airbnb\Project-3-Berlin-Airbnb-Ratings\Comments.xlsx', index=False)
+df_с = df_с[df_с['Comments'].str.contains('This is an automated posting') == False]
+df_с.to_excel(r'C:\Users\domashniy\Documents\GitHub\Project_3-Airbnb\Project-3-Berlin-Airbnb-Ratings\Comments.xlsx', index=False)
 
 df_с2 = df[['Listing ID', 'Comments']].copy()
+df_с2 = df_с2[df_с2['Comments'].str.contains('This is an automated posting') == False]
 df_с2.to_excel(r'C:\Users\domashniy\Documents\GitHub\Project_3-Airbnb\Project-3-Berlin-Airbnb-Ratings\Comments_full.xlsx', index=False)
 
